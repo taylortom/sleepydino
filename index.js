9577baca1a3f81d4e2fe07feda19c68c735b126c
Tom Taylor <hello@tomtaylor.name>
2020-08-29 16:31:01 +0100

    Refactor function

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const WEEKDAY = [0,1,2,3,4,5,6,7,8,19,20,21,22,23];
 const WEEKEND = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
 
 function run() {
-  const now = new Date();
-  const hours = now.getDay() > 0 && now.getDay() < 6 ? WEEKDAY : WEEKEND;
-  if(!hours.includes(now.getHours())) setTimeout(() => fetch("http://reactions.tomtaylor.name").then(run), INTERVAL);
+  setTimeout(async () => {
+    const now = new Date();
+    const hours = now.getDay() > 0 && now.getDay() < 6 ? WEEKDAY : WEEKEND;
+    if(!hours.includes(now.getHours())) await fetch("http://reactions.tomtaylor.name");
+    run();
+  }, INTERVAL);
 }
 
 module.exports = run();
